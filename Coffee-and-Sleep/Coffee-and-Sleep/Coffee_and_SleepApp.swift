2183c430884d4950cdb89551294cd0c240601026
Loi Pham <loitpham@gmail.com>
2021-06-01 18:42:42 -0700

    Got all the functionality working.

---
+++ Coffee-and-Sleep/Coffee-and-Sleep/Coffee_and_SleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_and_SleepApp.swift
+//  Coffee-and-Sleep
+//
+//  Created by Loi Pham on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_and_SleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
